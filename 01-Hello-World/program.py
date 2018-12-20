4dd735723d116a246766a602778557a3150dec57
owezzy <owenobezzy@gmail.com>
2018-12-20 16:37:46 +0300

    complete app 1-Hello world

---
+++ 01-Hello-World/program.py
@@ -0,0 +1,9 @@
+print('----------------------------------------------------')
+print('                    HELLO APP')
+print('----------------------------------------------------')
+print()
+
+user_text = input('what is your name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
